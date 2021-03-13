0210ac324a8eea2c013406dc6a67b14ef858a937
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-13 19:00:03 +0300

    Интегрирован scss

--- src/PrintablePhrase/PrintablePhrase.js
+++ src/PrintablePhrase/PrintablePhrase.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import style from './PrintablePhrase.module.css';
+import style from './PrintablePhrase.module.scss';
 
 
 function PrintablePhrase({phrase, delay}) {
