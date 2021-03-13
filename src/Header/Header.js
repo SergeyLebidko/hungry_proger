654a99d4f765d66d7a483e4b2c19f128db3b7670
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-13 17:17:26 +0300

    Компонент Header.js преобразован в функциональный

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,18 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 import HeaderCover from '../HeaderCover/HeaderCover';
 import HeaderCanvas from '../HeaderCanvas/HeaderCanvas';
 import {headerHeight} from '../App';
 
 
-class Header extends Component {
-    render() {
-        return (
-            <>
-                <HeaderCover headerHeight={headerHeight}/>
-                <HeaderCanvas headerHeight={headerHeight}/>
-            </>
-        )
-    }
+function Header() {
+    return (
+        <>
+            <HeaderCover headerHeight={headerHeight}/>
+            <HeaderCanvas headerHeight={headerHeight}/>
+        </>
+    );
 }
 
 export default Header;
