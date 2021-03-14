38d8f79390be3182d347a794862d8e4e65a82ab4
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-14 16:02:28 +0300

    Рефакторинг стилей. Удален Avatar.js

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Avatar from '../Avatar/Avatar';
 import style from './AboutMe.module.scss';
 import SimpleButton from '../SimpleButton/SimpleButton';
 
@@ -7,21 +6,25 @@ import SimpleButton from '../SimpleButton/SimpleButton';
 function AboutMe() {
     return (
         <div>
-            <p className={style.header}>Обо мне</p>
-            <div className={style.separator}/>
+            <div className={style.header}>
+                <h1>Обо мне</h1>
+                <div/>
+            </div>
             <div className={style.content}>
-                <Avatar/>
+                <img className={style.avatar} src="/images/avatar.jpg"/>
                 <div>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                         labore
+                    </p>
+                    <p>
                         et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
                         ut
                         aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
                         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
                         culpa qui officia deserunt mollit anim id est laborum.
                     </p>
-                    <SimpleButton text="Смотреть полностью" delay={0} action={e=>e}/>
+                    <SimpleButton text="Смотреть полностью" delay={0} action={e => e}/>
                 </div>
             </div>
         </div>
--- src/Avatar/Avatar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import style from './Avatar.module.scss';
-
-
-function Avatar() {
-    return <img className={style.avatar} src="/images/avatar.jpg"/>;
-}
-
-export default Avatar;
