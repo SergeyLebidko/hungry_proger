15e3d9d20455311b6b1f46e3549fbb814c938c60
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-14 12:51:04 +0300

    Создан компонент Avatar.js

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Avatar from '../Avatar/Avatar';
 import style from './Content.module.css';
 import {headerHeight} from '../App';
 
@@ -6,8 +7,11 @@ import {headerHeight} from '../App';
 function Content(){
     return (
         <div className={style.container} style={{top: `${headerHeight}px`}}>
-            <div className={style.cap1}></div>
-            <div className={style.cap2}></div>
+            <div className={style.cap1}/>
+            <div className={style.cap2}/>
+            <div className={style.content_block}>
+                <Avatar/>
+            </div>
         </div>
     )
 }
