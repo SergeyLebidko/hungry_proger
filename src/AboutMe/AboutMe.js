b5339a3647ca1275c49af3fd78ecfa0ec5853a1f
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-14 16:36:10 +0300

    Удален компонент Content.js

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import style from './AboutMe.module.scss';
 import SimpleButton from '../SimpleButton/SimpleButton';
+import {headerHeight} from '../App';
 
 
 function AboutMe() {
+    let inlineStyle = {top: `${headerHeight}px`};
     return (
-        <div>
+        <div className={style.container} style={inlineStyle}>
             <div className={style.header}>
                 <h1>Обо мне</h1>
                 <div/>
@@ -24,7 +26,7 @@ function AboutMe() {
                         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
                         culpa qui officia deserunt mollit anim id est laborum.
                     </p>
-                    <SimpleButton text="Смотреть полностью" delay={0} action={e => e}/>
+                    <SimpleButton text="Читать полностью" delay={0} action={e => e}/>
                 </div>
             </div>
         </div>
--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Header from './Header/Header';
 import TitleBlock from './TitleBlock/TitleBlock';
-import Content from './Content/Content';
 import style from './App.module.css';
+import AboutMe from "./AboutMe/AboutMe";
 
 export const headerHeight = window.innerHeight + 50;
 
@@ -12,7 +12,7 @@ function App() {
         <>
             <Header/>
             <TitleBlock/>
-            <Content/>
+            <AboutMe/>
         </>
     );
 }
