a605ec1d0ddf7e9c9290f438071081e0842d15c1
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-18 13:22:58 +0300

    Рефакторинг Article.js

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -5,13 +5,14 @@ import style from './Article.module.scss';
 
 
 function Article({title, content, history}) {
+    let hasHeader = content.header.length > 0;
     return (
         <div className={style.container}>
             <div className={style.cap}/>
             <div className={style.content}>
                 <div className={style.title}>{title}</div>
                 <div className={style.separator}/>
-                <div className={style.header}>{content.header.map(value => <p>{value}</p>)}</div>
+                {hasHeader ? '': <div className={style.header}>{content.header.map(value => <p>{value}</p>)}</div>}
                 <div>{content.body.map(value => <p>{value}</p>)}</div>
                 <SimpleButton text="На главную" delay={0} action={() => history.push('/')}/>
             </div>
