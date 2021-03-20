b2e2d6b40079e253adabcc50991762b6f668ba69
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-20 20:16:52 +0300

    Правка ошибок

--- src/PrintablePhrase/PrintablePhrase.js
+++ src/PrintablePhrase/PrintablePhrase.js
@@ -2,11 +2,6 @@ import React, {useState, useEffect} from 'react'
 import style from './PrintablePhrase.module.scss';
 
 
-let printablePhraseStorage = {
-    text: null,
-    hasCursor: null
-}
-
 function PrintablePhrase({phrase, delay}) {
     let [text, setText] = useState('');
     let [hasCursor, setHasCursor] = useState(false);
@@ -35,7 +30,6 @@ function PrintablePhrase({phrase, delay}) {
         }
 
         return () => {
-            printablePhraseStorage = {text, hasCursor}
             clearInterval(interval);
             clearTimeout(timeout);
 
