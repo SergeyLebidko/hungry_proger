d93c38058bc5400faf63b84400d2bcaf0614ca44
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-26 19:03:32 +0300

    В HeaderCanvas.js опробован эффект разлета

--- src/HeaderCanvas/HeaderCanvas.js
+++ src/HeaderCanvas/HeaderCanvas.js
@@ -10,63 +10,31 @@ function HeaderCanvas() {
         paper.setup(_canvas.current);
         let {view, Path, Point} = paper;
 
-        const W = view.size.width;
-        const H = view.size.height;
+        let data = [];
+        let circle, vector;
+        view.onClick = function (event) {
+            for (let index = 0; index < 20; index++) {
+                circle = new Path.Circle(event.point, 15);
+                circle.fillColor = 'white';
+                circle.opacity = 0.3;
 
-        let segments = [];
-        segments.push(new Point(W, H));
-        segments.push(new Point(0, H));
-        segments.push(new Point(0, H * 0.8));
-
-        let baseLine = H * 0.8;
-
-        let factor = 20;
-        for (let index = 1; index < 10; index++) {
-            segments.push(new Point(W / 10 * index, baseLine + factor));
-            factor *= -1;
-        }
-        segments.push(new Point(W, H * 0.8));
-
-        let path = new Path(segments);
-        path.closed = true;
-
-        let topLeft = new Point(W / 2, baseLine - 30);
-        let bottomRight = new Point(W / 2, H);
-
-        path.fillColor = {
-            gradient: {
-                stops: ['blue', 'deepskyblue']
-            },
-            origin: topLeft,
-            destination: bottomRight
-        };
-        path.smooth({from: 2, to: 12});
-
-        function getRandomArr() {
-            let result = [];
-            for (let index = 0; index < 11; index++) {
-                result.push(1 - (2 * (index % 2)));
+                vector = Point.random().multiply(5 + Math.random() * 8);
+                vector.angle = 180 - Math.random() * 360;
+                data.push({circle, vector, onScreen: true});
             }
-            return result;
         }
-
-        let waves = [
-            {
-                deltaList: getRandomArr(),
-                baseLine,
-                wave: path
-            }
-        ];
 
         view.onFrame = function () {
-            waves.forEach(({deltaList, baseLine, wave}) => {
-                for (let index = 2; index <= 12; index++) {
-                    wave.segments[index].point.y += deltaList[index - 2];
-                    if ((wave.segments[index].point.y > (baseLine + 30)) || (wave.segments[index].point.y < (baseLine - 30))) {
-                        deltaList[index - 2] *= -1;
-                    }
+            for (let index = 0; index < data.length; index++) {
+                data[index].circle.position.x += data[index].vector.x;
+                data[index].circle.position.y += data[index].vector.y;
+                if (!data[index].circle.isInside(view.bounds)) {
+                    data[index].circle.remove();
+                    data[index].onScreen = false;
                 }
-            });
+            }
+
+            data = data.filter(value => value.onScreen);
         }
 
     }, []);
