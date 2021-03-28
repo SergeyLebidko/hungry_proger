faa57986a937668db3a948ecf824ed2a0fde4e57
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-28 18:52:28 +0300

    Создана верстка для UpMenu.js

--- src/demo_components/up_menu_demo/UpMenu/UpMenu.js
+++ src/demo_components/up_menu_demo/UpMenu/UpMenu.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import style from './UpMenu.module.scss';
 
 function UpMenu({items}) {
-    let inlineStyle = {backgroundImage: 'url(/images/demo_components/up_menu_demo/back_menu.png)'}
+    let containerInlineStyle = {backgroundImage: 'url(/images/demo_components/up_menu_demo/back_menu.png)'}
     return (
-        <div className={style.container} style={inlineStyle}>
-            <ul>
+        <div className={style.container} style={containerInlineStyle}>
+            <img src={"/images/demo_components/up_menu_demo/logo.png"}/>
+            <div className={style.menu_button}>
+                <div/>
+                <div/>
+                <div/>
+                <div/>
+                <div/>
+                <div/>
+            </div>
+            <ul className={style.horizontal}>
                 {items.map((value, index) => <li key={index}>{value}</li>)}
             </ul>
         </div>
