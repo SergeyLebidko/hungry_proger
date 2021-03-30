569aee71386a0d9e1d81512279964fa2a4f5cb4f
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-30 14:27:03 +0300

    Добавлен компонент Logo

--- src/demo_components/menu_demo/Menu/Menu.js
+++ src/demo_components/menu_demo/Menu/Menu.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Logo from '../../Logo/Logo';
 import style from './Menu.module.scss';
 
 function Menu({minimizeFlag}) {
     let inlineStyle = {backgroundImage: 'url(/images/demo_components/menu_demo/back_menu.png)'}
     return (
         <div className={`${style.container} ${minimizeFlag ? style.minimize_height : style.standard_height}`}
-             style={inlineStyle}/>
+             style={inlineStyle}>
+            <Logo minimizeFlag={minimizeFlag}/>
+        </div>
     );
 }
 
