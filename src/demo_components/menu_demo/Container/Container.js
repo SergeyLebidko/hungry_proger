115a0f6dacd2ef953aac50e1775d3ba409819e9e
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-30 13:04:26 +0300

    Для меню создан контейнер

--- src/demo_components/menu_demo/Container/Container.js
+++ src/demo_components/menu_demo/Container/Container.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import style from './Container.module.scss'
 
+const backgroundImage = 'url(/images/demo_components/menu_demo/back_container.png';
+const backgroundGradient = 'radial-gradient(circle, transparent, rgba(30, 30, 30, 0.6))';
 
 export function Container() {
+    let inlineStyle = {
+        backgroundImage: `${backgroundGradient}, ${backgroundImage}`
+    }
     return (
-        <div>Здесь будет компонент с меню</div>
+        <div className={style.container} style={inlineStyle}/>
     )
 }
