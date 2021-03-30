7d5f696d44ee7e53ce95daeafbc63ff5ba053b74
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-30 17:27:59 +0300

    Изменена кнопка меню

--- src/demo_components/menu_demo/Button/Button.js
+++ src/demo_components/menu_demo/Button/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import style from './Button.module.scss';
 
-function Button({minimizeFlag, clickHandler}){
-    let className = style.container + ' ' + (minimizeFlag ? style.minimize_size : style.normal_size);
+function Button({minimizeFlag, clickHandler, hasOpen}) {
+    let className = style.container + ' ' + (minimizeFlag ? style.minimize_size : style.normal_size) + ' ' + (hasOpen ? style.open : style.closed);
     return (
         <div className={className} onClick={clickHandler}>
             <div/>
