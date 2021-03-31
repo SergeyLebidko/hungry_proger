8c964f35d1badc38009976311f3bc4a39567739c
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-31 17:48:59 +0300

    Реализовано перемещение фона заголовка при движении мышью

--- src/demo_components/gallery_demo/Header/Header.js
+++ src/demo_components/gallery_demo/Header/Header.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
+import {withRouter} from 'react-router-dom';
 import style from './Header.module.scss';
 
-function Header() {
+function Header({history}) {
+    let [xPos, setXPos] = useState(50);
+    let [yPos, setYPos] = useState(50);
+    let container = useRef(null);
+
+    function moveHandler(event) {
+        let {clientX, clientY} = event;
+        setXPos(40 * clientX / container.current.clientWidth);
+        setYPos(40 * clientY / container.current.clientHeight);
+    }
+
     let captionStyle = {backgroundImage: 'url(/images/demo_components/gallery_demo/back.png)'}
-    let containerStyle = {backgroundImage: 'url(/images/demo_components/gallery_demo/header.jpg)'}
+    let containerStyle = {
+        backgroundImage: 'url(/images/demo_components/gallery_demo/header.jpg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '115% auto',
+        backgroundPosition: `${xPos}% ${yPos}%`
+    }
     return (
-        <div className={style.container} style={containerStyle}>
+        <div className={style.container} style={containerStyle} onMouseMove={moveHandler} ref={container}>
+            <div className={style.close_button} onClick={() => history.push('/')}>
+                <div/>
+                <div/>
+            </div>
             <h2 style={captionStyle}>
                 Здесь представлены компоненты с изображениями, организованные в небольшую галерею.
                 Чтобы вернуться на главную страницу - кликните на крестик в правом верхнем углу.
@@ -14,4 +34,4 @@ function Header() {
     )
 }
 
-export default Header;
+export default withRouter(Header);
