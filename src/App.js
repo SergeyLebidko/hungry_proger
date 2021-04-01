fa5378f7b430d5f32aadbd8001f37e164cda563e
SergeyLebidko <it.sergeyler@mail.ru>
2021-04-01 18:54:52 +0300

    Создан контейнер для демо табличного компонента

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import HeaderCanvas from './HeaderCanvas/HeaderCanvas';
 // Импорты demo-компонентов
 import {Container as MenuDemoContainer} from './demo_components/menu_demo/Container/Container';
 import {Container as GalleryDemoContainer} from './demo_components/gallery_demo/Container/Container';
+import {Container as TableDemoContainer} from './demo_components/table_demo/Container/Container';
 
 export const Context = React.createContext(store);
 const DEMO_PATH = '/demo';
@@ -30,6 +31,11 @@ const DEMO_DATA = [
         title: 'Галерея',
         href: 'gallery',
         component: <GalleryDemoContainer/>
+    },
+    {
+        title: 'Таблица',
+        href: 'table',
+        component: <TableDemoContainer/>
     }
 ];
 
---
+++ src/demo_components/table_demo/Container/Container.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Container.module.scss';
+
+export function Container(){
+    return (
+        <div>
+            Здесь будет таблица
+        </div>
+    )
+}
