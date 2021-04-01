675f17d9b383fe29ea7b057421d557ae48b191fd
SergeyLebidko <it.sergeyler@mail.ru>
2021-04-01 14:47:53 +0300

    Добавлен прелоадер для изображений в галерее

--- src/demo_components/gallery_demo/Card/Card.js
+++ src/demo_components/gallery_demo/Card/Card.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Preloader from '../Preloader/Preloader';
 import style from './Card.module.scss';
 
 export const left_pos = 'l';
@@ -6,6 +7,15 @@ export const right_pos = 'r';
 
 function Card({item, clickHandler}) {
     let [pos, setPos] = useState(right_pos);
+    let [stage, setStage] = useState(0);
+
+    function onLoadHandler() {
+        setStage(stage + 1);
+    }
+
+    function preloaderDisabler() {
+        setStage(3);
+    }
 
     let containerClassName = style.container;
     if (pos === left_pos) containerClassName += (' ' + style.left_pos);
@@ -20,8 +30,8 @@ function Card({item, clickHandler}) {
     classForRightControl = pos === right_pos ? style.selected : '';
     return (
         <div className={containerClassName}>
-            <img src={filename1} onClick={() => clickHandler(filename1)}/>
-            <img src={filename2} onClick={() => clickHandler(filename2)}/>
+            <img src={filename1} onClick={() => clickHandler(filename1)} onLoad={onLoadHandler}/>
+            <img src={filename2} onClick={() => clickHandler(filename2)} onLoad={onLoadHandler}/>
             <div className={style.title}>
                 <h1>{item.title}</h1>
             </div>
@@ -29,6 +39,7 @@ function Card({item, clickHandler}) {
                 <div className={classForRightControl} onClick={() => setPos(right_pos)}>1</div>
                 <div className={classForLeftControl} onClick={() => setPos(left_pos)}>2</div>
             </div>
+            {stage < 3 ? <Preloader hasExit={stage === 2} disabler={preloaderDisabler}/> : ''}
         </div>
     )
 }
---
+++ src/demo_components/gallery_demo/Preloader/Preloader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './Preloader.module.scss';
+
+function Preloader({hasExit, disabler}) {
+    let containerStyle = {backgroundImage: 'url(/images/demo_components/gallery_demo/back_preloader.png)'}
+    let containerClass = style.container;
+    if (hasExit) {
+        containerClass += (' ' + style.exit);
+        setTimeout(() => disabler(), 1950);
+    }
+    return (
+        <div className={containerClass} style={containerStyle}>
+            <div/>
+            <div/>
+        </div>
+    )
+}
+
+export default Preloader;
