1357335111b1724c0e96bb9c5643cb2780092285
SergeyLebidko <it.sergeyler@mail.ru>
2021-04-05 16:40:37 +0300

    В Tool.js добавлена возможность перемещения стрелками

--- src/demo_components/table_demo/Tool/Tool.js
+++ src/demo_components/table_demo/Tool/Tool.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import style from './Tool.module.scss';
 
 function Tool({addCopyFlag, addCopyLastFlag, removeFlag, removeAllFlag, addEmptyHandler, addCopyLastHandler, addCopyHandler, removeHandler, removeAllHandler}) {
@@ -9,46 +9,54 @@ function Tool({addCopyFlag, addCopyLastFlag, removeFlag, removeAllFlag, addEmpty
     let removeClass = removeFlag ? style.enabled_remove : style.disabled;
     let removeAllClass = removeAllFlag ? style.enabled_remove : style.disabled;
 
+    // Устанавливаем рефы
+    let addEmptyRef = useRef(null);
+    let addCopyLastRef = useRef(null);
+    let addCopyRef = useRef(null);
+    let removeRef = useRef(null);
+    let removeAllRef = useRef(null);
+
+    function keyDownHandler(prevRef, nextRef, event, action) {
+        if (event.code === 'ArrowLeft') prevRef.current.focus();
+        if (event.code === 'ArrowRight') nextRef.current.focus();
+        if (event.code === 'Enter' || event.code === 'Space') action();
+    }
+
     return (
         <ul className={style.container}>
             <li className={style.enabled_add}
                 onClick={addEmptyHandler}
-                onKeyDown={e => {
-                    if (e.code === 'Enter' || e.code === 'Space') addEmptyHandler()
-                }}
-                tabIndex={1}>
+                onKeyDown={e => keyDownHandler(removeAllRef, addCopyLastRef, e, addEmptyHandler)}
+                tabIndex={1}
+                ref={addEmptyRef}>
                 Добавить новую строку
             </li>
             <li className={addCopyLastClass}
                 onClick={addCopyLastHandler}
-                onKeyDown={e => {
-                    if (e.code === 'Enter' || e.code === 'Space') addCopyLastHandler()
-                }}
-                tabIndex={1}>
+                onKeyDown={e => keyDownHandler(addEmptyRef, addCopyRef, e, addCopyLastHandler)}
+                tabIndex={1}
+                ref={addCopyLastRef}>
                 Добавить копию последней строки
             </li>
             <li className={addCopyClass}
                 onClick={addCopyHandler}
-                onKeyDown={e => {
-                    if (e.code === 'Enter' || e.code === 'Space') addCopyHandler()
-                }}
-                tabIndex={1}>
+                onKeyDown={e => keyDownHandler(addCopyLastRef, removeRef, e, addCopyHandler)}
+                tabIndex={1}
+                ref={addCopyRef}>
                 Добавить копию выделенной строки
             </li>
             <li className={removeClass}
                 onClick={removeHandler}
-                onKeyDown={e => {
-                    if (e.code === 'Enter' || e.code === 'Space') removeHandler()
-                }}
-                tabIndex={1}>
+                onKeyDown={e => keyDownHandler(addCopyRef, removeAllRef, e, removeHandler)}
+                tabIndex={1}
+                ref={removeRef}>
                 Удалить выделенную строку
             </li>
             <li className={removeAllClass}
                 onClick={removeAllHandler}
-                onKeyDown={e => {
-                    if (e.code === 'Enter' || e.code === 'Space') removeAllHandler()
-                }}
-                tabIndex={1}>
+                onKeyDown={e => keyDownHandler(removeRef, addEmptyRef, e, removeAllHandler)}
+                tabIndex={1}
+                ref={removeAllRef}>
                 Удалить все строки
             </li>
         </ul>
