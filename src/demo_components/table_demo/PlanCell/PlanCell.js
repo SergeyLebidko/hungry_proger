2019ee9dbfb3da90d4c0a3b73f8bb59aea2dd9af
SergeyLebidko <it.sergeyler@mail.ru>
2021-04-05 15:12:28 +0300

    Исправлен баг с неправильной расстановкой флажков при удалении строки

--- src/demo_components/table_demo/PlanCell/PlanCell.js
+++ src/demo_components/table_demo/PlanCell/PlanCell.js
@@ -1,17 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import style from './PlanCell.module.scss';
 
 function PlanCell({plan, rowIndex, changePlanHandler}) {
-    let [inputValue, setInputValue] = useState(plan);
-
-    function changeHandler() {
-        setInputValue(!inputValue);
-        changePlanHandler(!inputValue, rowIndex);
-    }
 
     return (
         <td className={style.container}>
-            <input type={"checkbox"} value={plan} checked={inputValue} onChange={changeHandler} tabIndex={-1}/>
+            <input type={"checkbox"}
+                   checked={plan}
+                   onChange={() => changePlanHandler(!plan, rowIndex)}
+                   tabIndex={-1}
+            />
         </td>
     );
 }
