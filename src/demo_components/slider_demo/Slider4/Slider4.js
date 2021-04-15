3d0945ff5d8e1e0eda45e7c7efd0d26382a98b66
SergeyLebidko <it.sergeyler@mail.ru>
2021-04-15 16:08:46 +0300

    Созданы стили для Slider4.js

--- src/demo_components/slider_demo/Slider4/Slider4.js
+++ src/demo_components/slider_demo/Slider4/Slider4.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import $ from 'jquery';
 import Preloader, {darkTheme} from '../Preloader/Preloader';
 import {createTouchSlideProps, modeController, mode2, mode3} from '../sliderUtil';
@@ -13,12 +13,34 @@ for (let index = 0; index < files.length; index++) {
 }
 
 function Slider4({mode, imgLoadHandler}) {
+    let [pos, setPos] = useState(0);
+
+    let contentRef = useRef(null);
+
+    function toVisible($element) {
+        $(`img.${style.outer_img_clear}`, $element).removeClass(style.outer_img_clear).addClass(style.outer_img_blur);
+        $(`.${style.hide}`, $element).removeClass(style.hide).addClass(style.visible);
+    }
+
+    function toHide($element) {
+        $(`img.${style.outer_img_blur}`, $element).removeClass(style.outer_img_blur).addClass(style.outer_img_clear);
+        $(`.${style.hide}`, $element).removeClass(style.visible).addClass(style.hide);
+    }
+
+    // При запуске слайдера сразу же применяем эффект к его первому элементу
+    useEffect(() => {
+        toVisible($(`.${style.wrap}`, contentRef.current).eq(pos));
+    }, []);
+
     let images = [], index = 0;
     for (let item of items) {
         images.push(
-            <div key={index}>
-                <img src={`/images/demo_components/slider_demo/${item.file}.jpg`} onLoad={imgLoadHandler}/>
-                <div>
+            <div key={index} className={style.wrap} style={{left: `${100 * index}%`}}>
+                <img className={style.outer_img_clear}
+                     src={`/images/demo_components/slider_demo/${item.file}.jpg`}
+                     onLoad={imgLoadHandler}
+                />
+                <div className={style.hide}>
                     <img src={`/images/demo_components/slider_demo/${item.file}.jpg`} onLoad={imgLoadHandler}/>
                     <h3>{item.title}</h3>
                 </div>
@@ -27,10 +49,16 @@ function Slider4({mode, imgLoadHandler}) {
         index++;
     }
 
+    let leftArrow = <div/>;
+    let rightArrow = <div/>;
+
+    let arrowBlock = <div className={style.arrow_block}>{leftArrow}{rightArrow}</div>
+
     return (
         <div className={style.container}>
-            <div className={style.content}>
+            <div className={style.content} ref={contentRef}>
                 {images}
+                {arrowBlock}
                 {mode !== mode3 ? <Preloader theme={darkTheme} hasDeactivateProcess={mode === mode2}/> : ''}
             </div>
         </div>
