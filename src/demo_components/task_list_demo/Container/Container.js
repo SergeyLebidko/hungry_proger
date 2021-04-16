adff4f1ca1c7863c49f35724aed5cc7e0645ff2f
SergeyLebidko <it.sergeyler@mail.ru>
2021-04-16 16:26:08 +0300

    Удалены пресеты цветов

--- src/demo_components/task_list_demo/Container/Container.js
+++ src/demo_components/task_list_demo/Container/Container.js
@@ -3,15 +3,6 @@ import Description from '../Description/Description';
 import CategoryCreator from '../CategoryCreator/CategoryCreator';
 import style from './Container.module.scss';
 
-export const colorPresets = [
-    'rgb(27, 133, 250)',
-    'rgb(66, 218, 31)',
-    'rgb(255, 85, 40)',
-    'rgb(39, 160, 223)',
-    'rgb(199, 19, 255)',
-    'rgb(47, 56, 83)'
-];
-
 export function Container() {
     let [data, setData] = useState([]);
 
