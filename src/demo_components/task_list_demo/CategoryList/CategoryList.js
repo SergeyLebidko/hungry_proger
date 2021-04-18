f82c556351eb77efd7a83dc808b977a0cccca78c
SergeyLebidko <it.sergeyler@mail.ru>
2021-04-18 20:05:00 +0300

    Добавлена возможность прокрутки вбок для списка задач

--- src/demo_components/task_list_demo/CategoryList/CategoryList.js
+++ src/demo_components/task_list_demo/CategoryList/CategoryList.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import Category from '../Category/Category';
 import style from './CategoryList.module.scss';
 
 function CategoryList() {
+    let [hasSideScroll, setHasSideScroll] = useState(false);
+    let containerRef = useRef(null);
+    let mouseLine = useRef(null);
 
     // Код-заглушка. Должен быть удален
     let categoryList = [];
@@ -10,8 +13,31 @@ function CategoryList() {
         categoryList.push(<Category key={index}/>);
     }
 
+    function mouseDownHandler(event) {
+        mouseLine.current = event.clientX;
+        setHasSideScroll(true);
+    }
+
+    function mouseUpHandler() {
+        mouseLine.current = null;
+        setHasSideScroll(false);
+    }
+
+    function mouseMoveHandler(event) {
+        if (mouseLine.current === null) return;
+        let deltaX = mouseLine.current - event.clientX;
+        mouseLine.current = event.clientX;
+        containerRef.current.scrollLeft += deltaX;
+    }
+
+    let containerStyle = hasSideScroll ? {cursor: 'all-scroll'} : {};
     return (
-        <div className={style.container}>
+        <div className={style.container}
+             onMouseDown={mouseDownHandler}
+             onMouseUp={mouseUpHandler}
+             onMouseMove={mouseMoveHandler}
+             style={containerStyle}
+             ref={containerRef}>
             {categoryList}
         </div>
     );
