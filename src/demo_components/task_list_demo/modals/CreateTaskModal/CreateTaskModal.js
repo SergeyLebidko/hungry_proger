23d6144db0743fcdd76cfae39357f839a91d141d
SergeyLebidko <it.sergeyler@mail.ru>
2021-04-22 15:40:51 +0300

    RadioSelector внедрен в CreateTaskModal.js

--- src/demo_components/task_list_demo/modals/CreateTaskModal/CreateTaskModal.js
+++ src/demo_components/task_list_demo/modals/CreateTaskModal/CreateTaskModal.js
@@ -1,15 +1,15 @@
 import React, {useState, useRef} from 'react';
 import {ErrorController, getTaskTitles, useFocusAndStopErrorEffect, getChangeHandler} from '../modalUtils';
+import RadioSelector from '../RadioSelector/RadioSelector';
 import style from './CreateTaskModal.module.scss';
 
-function CreateTaskModal({maxLen, categoryList, defaultCategoryId, hideHandler, createHandler}) {
+function CreateTaskModal({maxLen, categoryList, hideHandler, createHandler}) {
     let [value, setValue] = useState('');
     let [error, setError] = useState(null);
 
-    let [radioValue, setRadioValue] = useState(defaultCategoryId || categoryList[0].id)
-
     let inputRef = useRef(null);
     let errorRef = useRef(new ErrorController(setError, getTaskTitles(categoryList)));
+    let selectedCategory = useRef(categoryList[0].id);
 
     // Ставим фокус на поле ввода при монтировании компонента, при размонтировании - отключаем таймер показа ошибок
     useFocusAndStopErrorEffect(inputRef, errorRef);
@@ -17,7 +17,7 @@ function CreateTaskModal({maxLen, categoryList, defaultCategoryId, hideHandler, 
     function saveButtonHandler() {
         let finalValue = value.trim();
         if (errorRef.current.checkError(finalValue)) return;
-        createHandler(finalValue, radioValue);
+        createHandler(finalValue, selectedCategory.current);
     }
 
     return (
@@ -25,31 +25,11 @@ function CreateTaskModal({maxLen, categoryList, defaultCategoryId, hideHandler, 
             <div className={style.content}>
                 <input type={'text'} value={value} onChange={getChangeHandler(setValue, maxLen)} ref={inputRef}/>
                 {error !== null ? <div className={style.error_block}>{error}</div> : ''}
-                <div className={style.radio_selector}>
-                    <p>В какую категорию поместить задачу?</p>
-                    <table>
-                        <tbody>
-                        {categoryList.map(category => {
-                            return (
-                                <tr>
-                                    <td>
-                                        <input id={`category_${category.id}`}
-                                               type={'radio'}
-                                               name={'category'}
-                                               value={category.id}
-                                               checked={category.id === radioValue}
-                                               onChange={() => setRadioValue(category.id)}
-                                        />
-                                    </td>
-                                    <td>
-                                        <label htmlFor={`category_${category.id}`}>{category.title}</label>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                        </tbody>
-                    </table>
-                </div>
+                <RadioSelector title={'В какую категорию поместить задачу?'}
+                               categoryList={categoryList}
+                               startValue={categoryList[0].id}
+                               onChange={id => selectedCategory.current = id}
+                />
                 <div className={style.button_block}>
                     <input type={'button'} value={'Создать'} className={style.yes_btn} onClick={saveButtonHandler}/>
                     <input type={'button'} value={'Отмена'} className={style.no_btn} onClick={hideHandler}/>
