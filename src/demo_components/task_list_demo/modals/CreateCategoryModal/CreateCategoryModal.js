b51ea65a10b830bd8a8eb90e9d2bd0df6575d2eb
SergeyLebidko <it.sergeyler@mail.ru>
2021-04-22 15:23:21 +0300

    RadioSelector.js внедрен в CreateCategoryModal.js

--- src/demo_components/task_list_demo/modals/CreateCategoryModal/CreateCategoryModal.js
+++ src/demo_components/task_list_demo/modals/CreateCategoryModal/CreateCategoryModal.js
@@ -1,16 +1,17 @@
 import React, {useState, useRef} from 'react';
 import {ErrorController, getCategoryTitles, useFocusAndStopErrorEffect, getChangeHandler} from '../modalUtils';
 import style from './CreateCategoryModal.module.scss';
+import RadioSelector from "../RadioSelector/RadioSelector";
 
 export const toEndCategoryPlace = 'to_end';
 
-function CreateCategoryModal({maxLen, categoryList,  hideHandler, createHandler}) {
+function CreateCategoryModal({maxLen, categoryList, hideHandler, createHandler}) {
     let [value, setValue] = useState('');
     let [error, setError] = useState(null);
-    let [beforeCategory, setBeforeCategory] = useState('to_end');
 
     let inputRef = useRef(null)
     let errorRef = useRef(new ErrorController(setError, getCategoryTitles(categoryList)));
+    let selectedCategory = useRef(toEndCategoryPlace);
 
     // Ставим фокус на поле ввода при монтировании компонента, при размонтировании - отключаем таймер показа ошибок
     useFocusAndStopErrorEffect(inputRef, errorRef);
@@ -18,7 +19,7 @@ function CreateCategoryModal({maxLen, categoryList,  hideHandler, createHandler}
     function createButtonHandler() {
         let title = value.trim();
         if (errorRef.current.checkError(title)) return;
-        createHandler(title, beforeCategory);
+        createHandler(title, selectedCategory.current);
     }
 
     return (
@@ -27,43 +28,12 @@ function CreateCategoryModal({maxLen, categoryList,  hideHandler, createHandler}
                 <input type={'text'} value={value} onChange={getChangeHandler(setValue, maxLen)} ref={inputRef}/>
                 {error !== null ? <div className={style.error_block}>{error}</div> : ''}
                 {categoryList.length > 0 ?
-                    <div className={style.radio_selector}>
-                        <p>Поместить перед категорией:</p>
-                        <table>
-                            <tbody>
-                            {categoryList.map((value, index) =>
-                                <tr key={index}>
-                                    <td>
-                                        <input id={value.id}
-                                               type={'radio'}
-                                               name={'before_cat'}
-                                               value={value.id}
-                                               onChange={e => setBeforeCategory(e.target.value)}
-                                               checked={+beforeCategory === value.id}
-                                        />
-                                    </td>
-                                    <td>
-                                        <label htmlFor={value.id}>{value.title}</label>
-                                    </td>
-                                </tr>
-                            )}
-                            <tr>
-                                <td>
-                                    <input id={'to_end'}
-                                           type={'radio'}
-                                           name={'before_cat'}
-                                           value={toEndCategoryPlace}
-                                           onChange={e => setBeforeCategory(e.target.value)}
-                                           checked={beforeCategory === toEndCategoryPlace}
-                                    />
-                                </td>
-                                <td>
-                                    <label htmlFor={'to_end'}>поместить в конец</label>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                    <RadioSelector title={'Поместить перед категрией:'}
+                                   categoryList={categoryList}
+                                   additionalElement={{id: toEndCategoryPlace, title: 'Поместить в конец'}}
+                                   startValue={toEndCategoryPlace}
+                                   onChange={id => selectedCategory.current = id}
+                    />
                     : ''
                 }
                 <div className={style.buttons_block}>
