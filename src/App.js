9b2205a6c3627323023c9eea7663ec405760b62d
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-16 15:52:10 +0300

    Удален axios

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ import {store} from './store';
 import HeaderCover from './HeaderCover/HeaderCover';
 import HeaderCanvas from './HeaderCanvas/HeaderCanvas';
 
+// Импорты констант - контента
+import {aboutMe, contacts, projects, skills, skillsDetail} from './content';
+
 // Импорты demo-компонентов
 import {Container as MainMenuDemoContainer} from './demo_components/main_menu_demo/Container/Container';
 import {Container as GalleryDemoContainer} from './demo_components/gallery_demo/Container/Container';
@@ -68,38 +71,23 @@ function App() {
         }, 600);
     });
 
-    let [aboutMeContent, setAboutMeContent] = useState(null);
-    let [projectsContent, setProjectsContent] = useState(null);
-    let [skillsList, setSkillsList] = useState(null);
-    let [contactsList, setContactsList] = useState(null);
-    let [skillsDetail, setSkillsDetail] = useState(null);
-
-    useEffect(() => {
-        axios.get('./content/about_me.txt').then(response => setAboutMeContent(response.data));
-        axios.get('./content/projects.txt').then(response => setProjectsContent(response.data));
-        axios.get('./content/skills.txt').then(response => setSkillsList(response.data));
-        axios.get('./content/contacts.txt').then(response => setContactsList(response.data));
-        axios.get('./content/skills_detail.txt').then(response => setSkillsDetail(response.data));
-        axios.get('./content/skills_detail.txt').then(response => setSkillsDetail(response.data));
-    }, []);
-
     return (
         <Switch>
             <Route exact path="/">
                 <HeaderCover/>
                 <HeaderCanvas/>
                 <TitleBlock windowSize={windowSize}/>
-                <AboutMe windowSize={windowSize} content={aboutMeContent}/>
-                <Skills windowSize={windowSize} content={skillsList}/>
+                <AboutMe windowSize={windowSize} content={aboutMe}/>
+                <Skills windowSize={windowSize} content={skills}/>
                 <Demos windowSize={windowSize} content={DEMO_DATA}/>
-                <Project windowSize={windowSize} content={projectsContent}/>
-                <Contacts windowSize={windowSize} content={contactsList}/>
+                <Project windowSize={windowSize} content={projects}/>
+                <Contacts windowSize={windowSize} content={contacts}/>
             </Route>
             <Route exact path="/about_me">
-                {aboutMeContent === null ? <Redirect to="/"/> : <Article content={aboutMeContent} title="Обо мне"/>}
+                <Article content={aboutMe} title="Обо мне"/>
             </Route>
             <Route exact path="/skills">
-                {aboutMeContent === null ? <Redirect to="/"/> : <Article content={skillsDetail} title="Технологии"/>}
+                <Article content={skillsDetail} title="Технологии"/>
             </Route>
             {DEMO_DATA.map(data => <Route exact path={`${DEMO_PATH}/${data.href}`} key={data.href}>
                 {data.component}
