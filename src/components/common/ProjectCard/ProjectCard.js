922efdf270be3170c903281d67c7f0f048283861
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-15 16:41:07 +0300

    v2 Завершен первый вариант верстки списка проектов

---
+++ src/components/common/ProjectCard/ProjectCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import "./ProjectCard.scss";
+
+function ProjectCard({data}) {
+    return (
+        <li className="project_card">
+            <a className="project_card__title_link" href={data.git} target="_blank" rel="noopener noreferrer">
+                {data.title}
+            </a>
+            <p className="project_card__description">{data.description}</p>
+            <ul className="project_card__tech_list">
+                {data.tech.map(value => <li key={value}>{value}</li>)}
+            </ul>
+        </li>
+    );
+}
+
+ProjectCard.propTypes = {
+    data: PropTypes.shape({
+        git: PropTypes.string,
+        title: PropTypes.string,
+        description: PropTypes.string,
+        tech: PropTypes.arrayOf(PropTypes.string)
+    })
+}
+
+export default ProjectCard;
--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -1,10 +1,21 @@
 import React from "react";
+import data from "../../../content/data.json";
 import "./Projects.scss";
+import ProjectCard from "../../common/ProjectCard/ProjectCard";
 
 function Projects() {
     return (
         <section className="projects">
-            Проекты
+            <div className="projects__content">
+                <h1 className="section_header">Мои проекты</h1>
+                <p className="projects__description">
+                    Здесь приведена для примера лишь небольшая часть из более чем семидесяти выполненных мной
+                    в разное время pet-проектов.
+                </p>
+                <ul className="projects__project_list">
+                    {data.projects.map(projectData => <ProjectCard key={projectData.title} data={projectData}/>)}
+                </ul>
+            </div>
         </section>
     );
 }
