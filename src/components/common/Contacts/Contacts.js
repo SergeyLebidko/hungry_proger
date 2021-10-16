07ea34212ffbe15476d45a35a731cf8092c9b9f1
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-16 14:59:06 +0300

    v2 Заменены иконки в контактах

--- src/components/common/Contacts/Contacts.js
+++ src/components/common/Contacts/Contacts.js
@@ -1,24 +1,26 @@
 import React from "react";
 import "./Contacts.scss";
 import {GITHUB_HREF, MAIL_HREF, TELEGRAM_HREF} from "../../../constants/settings";
-import {FaGithub, FaTelegram, MdEmail} from "react-icons/all";
+import {ReactComponent as GuthubIcon} from "../../../content/icons/github.svg";
+import {ReactComponent as MailIcon} from "../../../content/icons/mail.svg";
+import {ReactComponent as TelegramIcon} from "../../../content/icons/telegram.svg";
 
 function Contacts() {
     return (
         <ul className="contacts">
             <li>
                 <a href={GITHUB_HREF} target="_blank" rel="noopener noreferrer">
-                    <FaGithub className="contacts__github"/>
+                    <GuthubIcon className="contacts__github"/>
                 </a>
             </li>
             <li>
                 <a href={MAIL_HREF} target="_blank" rel="noopener noreferrer">
-                    <MdEmail className="contacts__mail"/>
+                    <MailIcon className="contacts__mail"/>
                 </a>
             </li>
             <li>
                 <a href={TELEGRAM_HREF} target="_blank" rel="noopener noreferrer">
-                    <FaTelegram className="contacts__telegram"/>
+                    <TelegramIcon className="contacts__telegram"/>
                 </a>
             </li>
         </ul>
