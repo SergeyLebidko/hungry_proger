e57accb638ac76d7280a02e4b035a08566b8acaf
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-17 12:50:14 +0300

    v2 Добавлен прелоадер

--- src/components/pages/Main/Main.js
+++ src/components/pages/Main/Main.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from 'prop-types';
 import avatar from "../../../content/images/avatar.jpg";
+import Preloader from "../../common/Preloader/Preloader";
 import "./Main.scss";
 
 function Main({toAbout}) {
+    const [hasAvatarLoad, setHasAvatarLoad] = useState(false);
+
     return (
         <section className="main">
             <div className="main__content">
                 <div className="main__avatar_wrapper">
-                    <img src={avatar} className="main__avatar"/>
+                    <img src={avatar} className="main__avatar" onLoad={() => setHasAvatarLoad(true)}/>
+                    {!hasAvatarLoad && <Preloader/>}
                 </div>
                 <h1 className="main__name">Сергей Лебидко</h1>
                 <h3 className="main__profession">React-разработчик</h3>
