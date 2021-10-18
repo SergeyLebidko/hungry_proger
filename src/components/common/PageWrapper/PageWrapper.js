9fa92f8fb35f02461aea000c45a9374fff7baa9d
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-18 14:31:49 +0300

    v2 Создан компонент PageWrapper.js

---
+++ src/components/common/PageWrapper/PageWrapper.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./PageWrapper.scss";
+
+function PageWrapper({component, line}) {
+    let inline = {zIndex: 0, left: 0};
+
+    if (line !== null) inline = {...inline, zIndex: 1, left: `${line}vw`};
+
+    return (
+        <div className="page_wrapper" style={inline}>
+            {component}
+        </div>
+    );
+}
+
+PageWrapper.propTypes = {
+    component: PropTypes.elementType,
+    line: PropTypes.number
+}
+
+export default PageWrapper;
