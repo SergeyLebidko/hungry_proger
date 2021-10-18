45fed5a67826cc52ef1ebcb76bfeead332e0c2bc
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-18 15:29:54 +0300

    v2 Создан первый вариант слайдера

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,18 +10,58 @@ import PageWrapper from "../common/PageWrapper/PageWrapper";
 
 function App() {
     const [mode, setMode] = useState(MAIN_MODE);
+    const [line, setLine] = useState(0);
 
-    const toMain = () => setMode(MAIN_MODE);
-    const toAbout = () => setMode(ABOUT_MODE);
-    const toSkills = () => setMode(SKILLS_MODE);
-    const toProjects = () => setMode(PROJECTS_MODE);
+    const [nextMode, setNextMode] = useState(null);
+    const [nextLine, setNextLine] = useState(null);
+
+    const MODE_LIST = [MAIN_MODE, ABOUT_MODE, SKILLS_MODE, PROJECTS_MODE];
+
+    const switchMode = required => {
+        if (mode && nextMode) return;
+        if (required === mode) return;
+
+        let delta;
+        if (MODE_LIST.indexOf(mode) < MODE_LIST.indexOf(required)) {
+            setNextLine(100);
+            delta = -1;
+        } else {
+            setNextLine(-100);
+            delta = 1;
+        }
+
+        setNextMode(required);
+        for (let step = 0; step <= 100; step++) {
+            setTimeout(() => {
+                setLine(oldValue => oldValue + delta);
+                setNextLine(oldValue => oldValue + delta);
+                if (step === 100) {
+                    setMode(required);
+                    setLine(0);
+                    setNextMode(null);
+                    setNextLine(null);
+                }
+            }, step * 5);
+        }
+    }
+
+    const getLine = wrapperMode => {
+        if (wrapperMode === mode) return line;
+        if (wrapperMode === nextMode) return nextLine;
+        return null;
+    }
+
+    const toMain = () => switchMode(MAIN_MODE);
+    const toAbout = () => switchMode(ABOUT_MODE);
+    const toSkills = () => switchMode(SKILLS_MODE);
+    const toProjects = () => switchMode(PROJECTS_MODE);
 
     return (
         <main className="app">
-            {mode === MAIN_MODE && <PageWrapper component={<Main toAbout={toAbout}/>}/>}
-            {mode === ABOUT_MODE && <PageWrapper component={<About/>}/>}
-            {mode === SKILLS_MODE && <PageWrapper component={<Skills/>}/>}
-            {mode === PROJECTS_MODE && <PageWrapper component={<Projects/>}/>}
+            <PageWrapper line={getLine(MAIN_MODE)} component={<Main toAbout={toAbout}/>}/>
+            <PageWrapper line={getLine(ABOUT_MODE)} component={<About/>}/>
+            <PageWrapper line={getLine(SKILLS_MODE)} component={<Skills/>}/>
+            <PageWrapper line={getLine(PROJECTS_MODE)} component={<Projects/>}/>
             <Menu mode={mode} toMain={toMain} toAbout={toAbout} toSkills={toSkills} toProjects={toProjects}/>
         </main>
     );
