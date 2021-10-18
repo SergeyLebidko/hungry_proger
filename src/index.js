bca529b5f269d3075ffa4b137194ce08ac1f36d6
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-18 19:09:18 +0300

    v2 Устранен эффект мерцания при запуске приложения

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './components/App/App';
+import {applyPreset} from "./utils/utils";
+import {COLOR_PRESETS, L_COLORS} from "./constants/settings";
 import './styles/init.scss';
+
+applyPreset(COLOR_PRESETS[L_COLORS]);
 
 render(<App/>, document.getElementById('root'));
