dcc79f92961785252b86d40fc806a4dd386436a0
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-18 16:27:27 +0300

    v2 Завершен первый вариант слайдера в App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,17 +4,48 @@ import Main from "../pages/Main/Main";
 import About from "../pages/About/About";
 import Skills from "../pages/Skills/Skills";
 import Projects from "../pages/Projects/Projects";
-import PageWrapper from "../common/PageWrapper/PageWrapper";
+import PageWrapper, {
+    FRONT,
+    BACK,
+    RISE_FROM_LEFT,
+    RISE_FROM_RIGHT,
+    LEAVE_TO_LEFT,
+    LEAVE_TO_RIGHT
+} from "../common/PageWrapper/PageWrapper";
 import {MAIN_MODE, ABOUT_MODE, SKILLS_MODE, PROJECTS_MODE} from "../../constants/settings";
 import "./App.scss";
 
 function App() {
     const [mode, setMode] = useState(MAIN_MODE);
+    const [nextMode, setNextMode] = useState(null);
 
-    // const MODE_LIST = [MAIN_MODE, ABOUT_MODE, SKILLS_MODE, PROJECTS_MODE];
+    const MODE_LIST = [MAIN_MODE, ABOUT_MODE, SKILLS_MODE, PROJECTS_MODE];
 
     const switchMode = required => {
-        setMode(required);
+        if (mode && nextMode) return;
+        if (mode === required) return;
+
+        setNextMode(required);
+        setTimeout(() => {
+            setMode(required);
+            setNextMode(null);
+        }, 500);
+    }
+
+    const getDisplay = wrapperMode => {
+        if (nextMode) {
+            const direction = MODE_LIST.indexOf(nextMode) > MODE_LIST.indexOf(mode) ? 'l' : 'r';
+            if (direction === 'l') {
+                if (wrapperMode === mode) return LEAVE_TO_LEFT;
+                if (wrapperMode === nextMode) return RISE_FROM_RIGHT;
+            }
+            if (direction === 'r') {
+                if (wrapperMode === mode) return LEAVE_TO_RIGHT;
+                if (wrapperMode === nextMode) return RISE_FROM_LEFT;
+            }
+        }
+        if (wrapperMode === mode) return FRONT;
+        return BACK;
     }
 
     const toMain = () => switchMode(MAIN_MODE);
@@ -24,10 +55,10 @@ function App() {
 
     return (
         <main className="app">
-            <PageWrapper component={<Main toAbout={toAbout}/>}/>
-            <PageWrapper component={<About/>}/>
-            <PageWrapper component={<Skills/>}/>
-            <PageWrapper component={<Projects/>}/>
+            <PageWrapper display={getDisplay(MAIN_MODE)} component={<Main toAbout={toAbout}/>}/>
+            <PageWrapper display={getDisplay(ABOUT_MODE)} component={<About/>}/>
+            <PageWrapper display={getDisplay(SKILLS_MODE)} component={<Skills/>}/>
+            <PageWrapper display={getDisplay(PROJECTS_MODE)} component={<Projects/>}/>
             <Menu mode={mode} toMain={toMain} toAbout={toAbout} toSkills={toSkills} toProjects={toProjects}/>
         </main>
     );
--- src/components/common/PageWrapper/PageWrapper.js
+++ src/components/common/PageWrapper/PageWrapper.js
@@ -3,27 +3,29 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import "./PageWrapper.scss";
 
-const RISE_FROM_RIGHT = 'rfr';
-const LEAVE_TO_LEFT = 'ltl';
-const RISE_FROM_LEFT = 'rfl';
-const LEAVE_TO_RIGHT = 'ltr';
+export const BACK = 'b';
+export const FRONT = 'f';
+
+export const RISE_FROM_RIGHT = 'rfr';
+export const LEAVE_TO_LEFT = 'ltl';
+export const RISE_FROM_LEFT = 'rfl';
+export const LEAVE_TO_RIGHT = 'ltr';
 
 function PageWrapper({component, display}) {
-    const wrapperClasses = classNames(
-        "page_wrapper",
-        {
-            "rise_from_right_wrapper": display === RISE_FROM_RIGHT,
-            "leave_to_left_wrapper": display === LEAVE_TO_LEFT,
-            "rise_from_left_wrapper": display === RISE_FROM_LEFT,
-            "leave_to_right_wrapper": display === LEAVE_TO_RIGHT
-        }
-    );
+    const wrapperClasses = classNames({
+        "page_wrapper_back": display === BACK,
+        "page_wrapper_front": display === FRONT,
+        "rise_from_right_wrapper": display === RISE_FROM_RIGHT,
+        "leave_to_left_wrapper": display === LEAVE_TO_LEFT,
+        "rise_from_left_wrapper": display === RISE_FROM_LEFT,
+        "leave_to_right_wrapper": display === LEAVE_TO_RIGHT
+    });
 
     return <div className={wrapperClasses}>{component}</div>;
 }
 
 PageWrapper.propTypes = {
-    component: PropTypes.elementType,
+    component: PropTypes.object,
     display: PropTypes.string
 }
 
